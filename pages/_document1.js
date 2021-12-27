dd31f2cf858b2bfd75cf7340c6b06456180f9fa1
Veselin Hr. Trayanov <1.1@gmx.com>
2021-12-27 03:27:42 +0000

    Script REMOVE!

--- pages/_document1.js
+++ pages/_document1.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-// import Script from 'next/script'
+
 
 class MyDocument extends Document {
      static async getInitialProps(ctx) {
@@ -11,31 +11,9 @@ class MyDocument extends Document {
           return (
                <Html>
                     <Head />
-                    <body>
-                         {/* <div id="colibri_hype_container"
-                                   class="HYPE_document"
-                                   style={{
-                                        // backgroundColor:'red',
-                                        position: "absolute",
-                                        top: 0,
-                                        margin: 'auto',
-                                        position: 'relative',
-                                        width: '600px',
-                                        height: '400px',
-                                        overflow: 'hidden'
-                                   }}/> */}
-
-                                   
+                    <body>                                   
                          <Main />
                          <NextScript />
-
-                         {/* <Script
-                              // type="text/javascript"
-                              // charset="utf-8"
-                              src="colibri1.hyperesources/colibri_hype_generated_script.js?23464"
-                              strategy="afterInteractive"
-                         >
-                         </Script> */}
                     </body>
                </Html>
           )
